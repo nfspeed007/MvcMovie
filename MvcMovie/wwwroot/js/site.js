9b176d3d8c530c229d65242654db8d514d0a0f52
nfspeed007 <isr15001@byui.edu>
2021-03-06 17:16:59 -0700

    Mvc Movie - Week 9

--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Get the current year
+var year = new Date();
+var currentYear = year.getFullYear();
+
+document.getElementById('currentDate').innerHTML = currentYear;
+
